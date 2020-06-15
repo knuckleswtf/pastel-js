ab67ab9f46b9f4e418f14ae3afe3fe3dac89b03d
shalvah <diakon.ng@gmail.com>
2020-06-15 20:39:08 +0100

    Complete CLI code

--- cli.js
+++ cli.js
@@ -1,28 +1,13 @@
 #!/usr/bin/env node
 
-const arrayKeys = [
-    'language_tabs',
-    'toc_footers',
-    'includes',
-];
-
+const program = require('commander');
 program
     .name('Scribe')
     .version('0.0.1')
     .command('generate <source> [destination]')
-    .option('-m, --metadata <metadata>', "Override the values set in the frontmatter.", (metadata) => {
-        let overrides = metadata.map(item => {
-            let [key, value] = item.split('=');
-            if (arrayKeys.includes(key)) {
-                value = value.split(',');
-            }
-            overrides[key] = value;
-            return overrides;
-        })
-    })
     .description("Generate HTML/CSS/JS documentation from a Markdown file or source folder  containing Markdown + assets..")
-    .action(async (...args) => {
-        require('index')(...args);
+    .action(async (source, destination) => {
+        require('index')(source, destination);
     });
 
 program.parse(process.argv);
